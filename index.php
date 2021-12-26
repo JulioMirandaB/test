9fc01f37e994b712a6757334b50b99aa06abc848
Julio Miranda B <96454553+JulioMirandaB@users.noreply.github.com>
2021-12-25 20:08:03 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,98 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>The Smart Contract Ecosystem Map</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="css/jstyles.css" rel="stylesheet" />        
+        <link href="css/all.min.css" rel="stylesheet" />
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:wght@200;300;400&display=swap" rel="stylesheet">  
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
+		
+		<script type="text/javascript" src="js/jquery.zoomooz-helpers.js"></script>
+		<script type="text/javascript" src="js/jquery.zoomooz-anim.js"></script>
+		<script type="text/javascript" src="js/jquery.zoomooz-core.js"></script>
+		<script type="text/javascript" src="js/jquery.zoomooz-zoomTarget.js"></script>
+		<script type="text/javascript" src="js/jquery.zoomooz-zoomButton.js"></script>
+		<script type="text/javascript" src="js/jquery.zoomooz-zoomContainer.js"></script>
+		<script type="text/javascript" src="js/purecssmatrix.js"></script>
+		<script type="text/javascript" src="js/sylvester.src.stripped.js"></script>  
+		</head>
+    <body>
+        <!-- Navigation-->
+
+        
+        
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary flex-sm-nowrap flex-wrap">
+	    <div class="container-fluid">
+	        <button class="navbar-toggler flex-grow-sm-1 flex-grow-0 me-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbar5">
+	            <span class="navbar-toggler-icon"></span>
+	        </button>
+	        <span style="margin-left: 0.5rem;" class="navbar-brand flex-grow-1">Chainlink Labs &nbsp &nbsp &nbsp<i style="color: black; font-size: 1.9rem; opacity: 0.4; vertical-align: sub;" class="fab fa-github"></i></a></li></span>
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a style="margin-right: 0.5rem;" class="nav-link" href="#"><i style="font-size: 1.7rem; color: white; transform: rotate(-25deg); " class="far fa-moon"></i></a>
+                </li>
+            </ul>
+        </div>
+	</nav>
+
+
+
+        <!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-left text-white">
+                    <h3 class="fw-normal">The Smart Contract Ecosystem Map</h3>
+                <br>
+                    <p style="font-weight:100; font-size:0.95rem;">The ecosystem tools map outlines the entire smart contract developer ecosystem, and all of the tools and products smart contract developers may need to rely on.
+                <br>
+                <br>
+
+
+These resources are comprised of mostly software tools but also include other resource types such as learning, exchanges, and reference resources. The goal of this map is to provide you with an overview of the full blockchain developer ecosystem.</p>
+                </div>
+            </div>
+        </header>
+        
+        
+        
+        
+        
+        
+        
+        <!-- Section-->
+        <section style="background-color: white;" class="py-5" id="myWorkContent">
+	        
+	        <?php  
+		        
+ //======================================================================
+// HERE GOES THE MAP  
+//====================================================================== 
+		        
+		        include 'map.php';?>
+	        
+	        
+        </section>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
